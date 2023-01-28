726aeaa6746e9b8bc262cd085db01cb22351972e
Thomas Vincent <thomasvincent@gmail.com>
2023-01-28 00:36:50 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,31 @@
-resource "aws_cloudformation_stack" "dedicated_hosts" {
-  name = var.cf_stack_name
-  tags = var.tags
-
-  template_body = <<STACK
-{
-  "Resources" : {
-    "MyDedicatedHost": {
-      "Type" : "AWS::EC2::Host",
-      "Properties" : {
-          "AutoPlacement" : "${var.auto_placement}",
-          "AvailabilityZone" : "${var.availability_zone}",
-          "HostRecovery" : "${var.host_recovery}",
-          "InstanceType" : "${var.instance_type}"
-        }
-    }
-  },
-  "Outputs" : {
-    "HostID" : {
-      "Description": "Host ID",
-      "Value" : { "Ref" : "MyDedicatedHost" }
-    }
-  }
+variable "auto_placement" {
+  default = "on"
 }
-STACK
+
+variable "availability_zone" {
+  default = "us-east-1a"
+}
+
+variable "host_recovery" {
+  default = "on"
+}
+
+variable "instance_type" {
+  default = "c4.large"
+}
+
+variable "tags" {
+  default = {}
+}
+
+resource "aws_dedicated_host" "example" {
+  auto_placement  = var.auto_placement
+  availability_zone = var.availability_zone
+  host_recovery = var.host_recovery
+  instance_type = var.instance_type
+  tags = var.tags
+}
+
+output "host_id" {
+  value = aws_dedicated_host.example.id
 }
